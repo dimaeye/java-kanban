3a80cbe8229f1920f86b30afa1218e9bffd54716
dimaeye <dima.airman23@yandex.ru>
2022-12-16 03:30:22 +0300

    style: remove trash comments and add line breaks

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,6 @@ import java.util.List;
 
 public class Main {
 
-    /**
-     * Создайте 2 задачи, один эпик с 2 подзадачами, а другой эпик с 1 подзадачей.
-     * Распечатайте списки эпиков, задач и подзадач, через System.out.println(..)
-     * Измените статусы созданных объектов, распечатайте.
-     * Проверьте, что статус задачи и подзадачи сохранился, а статус эпика рассчитался по статусам подзадач.
-     * И, наконец, попробуйте удалить одну из задач и один из эпиков.
-     */
     private static final TaskManager<Task> taskManager = new TaskManagerImpl();
     private static final TaskManager<Epic> epicManager = new EpicManagerImpl();
     private static final TaskManager<Subtask> subtaskManager = new SubtaskManagerImpl();
@@ -26,19 +19,25 @@ public class Main {
     public static void main(String[] args) {
         createTasks();
         System.out.println("-".repeat(120));
+
         createEpics();
         System.out.println("-".repeat(120));
+
         changeTasksStatus();
         System.out.println("-".repeat(120));
+
         changeSubtasksStatusAndCheckEpicStatus();
         System.out.println("-".repeat(120));
+
         removeTask();
         System.out.println("-".repeat(120));
+
         removeEpic();
     }
 
     private static void createTasks() {
         System.out.println("Создаем 2 задачи");
+
         List<Task> tasks = new ArrayList<>();
         tasks.add(new Task(taskManager.getUniqueId(), "Задача 1", "Описание задачи 1"));
         tasks.add(new Task(taskManager.getUniqueId(), "Задача 2", "Описание задачи 2"));
@@ -51,9 +50,9 @@ public class Main {
 
     private static void createEpics() {
         System.out.println("Создаем один эпик с 2 подзадачами, а другой эпик с 1 подзадачей");
+
         Epic firstEpic = new Epic(epicManager.getUniqueId(), "Эпик 1", "Описание эпика 1");
         Epic secondEpic = new Epic(epicManager.getUniqueId(), "Эпик 2", "Описание эпика 2");
-
 
         List<Subtask> subtasks = new ArrayList<>();
         subtasks.add(
@@ -85,6 +84,7 @@ public class Main {
 
     private static void changeTasksStatus() {
         System.out.println("Изменяем статусы созданных задач");
+
         List<Task> tasks = taskManager.getAll();
         for (Task task : tasks) {
             task.setStatus(TaskStatus.IN_PROGRESS);
@@ -96,6 +96,7 @@ public class Main {
     private static void changeSubtasksStatusAndCheckEpicStatus() {
         System.out.println("Изменяем статусы созданных подзадач и проверяем, "
                 + "что статус эпика рассчитался по статусам подзадач");
+
         List<Subtask> subtasks = subtaskManager.getAll();
         for (Subtask subtask : subtasks) {
             subtask.setStatus(TaskStatus.DONE);
@@ -108,6 +109,7 @@ public class Main {
 
     private static void removeTask() {
         System.out.println("Удаляем одну из задач");
+
         List<Task> tasks = taskManager.getAll();
         taskManager.remove(tasks.get(0).getId());
         taskManager.getAll().forEach(System.out::println);
@@ -115,9 +117,11 @@ public class Main {
 
     private static void removeEpic() {
         System.out.println("Удаляем один из эпиков и проверяем список всех подзадач");
+
         List<Epic> epics = epicManager.getAll();
         epicManager.remove(epics.get(0).getId());
         epicManager.getAll().forEach(System.out::println);
+
         System.out.println("-".repeat(120));
         subtaskManager.getAll().forEach(System.out::println);
     }
