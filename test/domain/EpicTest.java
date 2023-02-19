3950fddf702ac5d2b19737e2b4e06cb9b9858d5f
dimaeye <dima.airman23@yandex.ru>
2023-02-19 11:21:16 +0300

    build: added junit dependencies

---
+++ test/domain/EpicTest.java
@@ -0,0 +1,44 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EpicTest {
+
+    @Test
+    void setStatus() {
+    }
+
+    @Test
+    void addRelatedTask() {
+    }
+
+    @Test
+    void removeRelatedTask() {
+    }
+
+    @Test
+    void getAllRelatedTasks() {
+    }
+
+    @Test
+    void removeAllRelatedTasks() {
+    }
+
+    @Test
+    void getTaskType() {
+    }
+
+    @Test
+    void verifyEpicStatus() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
